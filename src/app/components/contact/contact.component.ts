0eaaaa5c8ee037383a40a0c01533b342839f87a0
Raj Desai <rajdesai94@gmail.com>
2017-05-02 14:15:08 -0400

    Added formspree emailing service

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactUsService } from '../../services/contactus.service';
 
 @Component({
   selector: 'contact',
   templateUrl: './contact.component.html',
+  providers: [ContactUsService],
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
@@ -11,7 +13,27 @@ export class ContactComponent {
   subject: string;
   message: string;
 
-  constructor() {
+  constructor(private contactService: ContactUsService) {
+    this.name = '';
+    this.email = '';
+    this.subject = '';
+    this.message = '';
+  }
+
+  emailUs() {
+    let data = {
+      Name: this.name,
+      Email: this.email,
+      Subject: this.subject,
+      Message: this.message
+    };
+
+    this.contactService.sendEmail(data);
+    this.resetFields();
+    this.message = "Thanks for your message, we'll get back to you ASAP!"
+  }
+
+  resetFields() {
     this.name = '';
     this.email = '';
     this.subject = '';
---
+++ src/app/services/contactus.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch'
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class ContactUsService {
+  constructor(private http: Http) {
+    console.log('contact service initialized!');
+  }
+
+  sendEmail(data) {
+    let body = JSON.stringify(data);
+    console.log('data:', data);
+    console.log('Email body:', body);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post('https://formspree.io/rajdesai94@gmail.com', data, options)
+    .map((res: Response) => console.log('Res:',res))
+    .catch((error:any) => Observable.throw(error.json().error || 'Server error'))
+    .subscribe();
+  }
+
+}
