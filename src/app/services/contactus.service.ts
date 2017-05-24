90d078b35ae76f4169a3689f593daf51d0e6d432
Raj Desai <rajdesai94@gmail.com>
2017-05-24 12:18:06 -0400

    styling updates

--- src/app/services/contactus.service.ts
+++ src/app/services/contactus.service.ts
@@ -20,19 +20,4 @@ export class ContactUsService {
     .catch((error:any) => Observable.throw(error.json().error || 'Server error'))
     .subscribe();
   }
-
-  emailSubscribe(data) {
-    // req body needs "email_address" and "status" keys
-    let body = JSON.stringify(data);
-    let user = 'SRUJANA_SHOW:8eac5fb5bb095fb4dd533417e7e7ba10-us15';
-    let headers = new Headers({
-      'Content-Type': 'application/json',
-      'Data-Type': 'json'
-    });
-    let options = new RequestOptions({ headers: headers });
-    return this.http.post('https://us15.api.mailchimp.com/3.0/lists/ad9449264b/members&c=?', data, options)
-    .map((res: Response) => console.log('Res:',res))
-    .catch((error:any) => Observable.throw(error.json().error || 'Server error'))
-    .subscribe();
-  }
 }
